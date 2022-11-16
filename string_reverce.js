48077e214a185ef598476ce6042dfbcc0f741193
jinal bhanderi <jinalsangani123@gmail.com>
2022-11-16 11:21:26 +0530

    ok

---
+++ string_reverce.js
@@ -0,0 +1,11 @@
+var str="sun rise in east"
+for(i=str.length-1;i>=0;i--){
+    console.log(str[i]);
+}
+
+// // var s=str.split(" ")
+// console.log(s);
+// for(var i=s.length-1;i>=0;i--){
+//     console.log(str[i]);
+    
+// }
